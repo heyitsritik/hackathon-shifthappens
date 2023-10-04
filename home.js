7b46f7aa0b0afc9405fc554bb8a7f63ae75049f4
heyitsritik <itsritik942@gmail.com>
2023-10-04 12:40:39 +0530

    first

---
+++ home.js
@@ -0,0 +1,7 @@
+// echo "# storm_shifthappens >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/heyitsritik/storm_shifthappens
+// git push -u origin main
